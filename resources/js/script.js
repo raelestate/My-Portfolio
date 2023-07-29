74d894a8bab3198b93f4b9430b8d1e0ebdcdd195
RaelEstate <rhyllneri@gmail.com>
2023-07-30 01:20:55 +0800

    jul 30

--- resources/js/script.js
+++ resources/js/script.js
@@ -55,3 +55,54 @@ var TxtType = function(el, toRotate, period) {
         document.body.appendChild(css);
     };
 
+    
+var container = document.getElementById("photopics");
+
+// Loop 20 times
+for (var i = 1; i < 202; i++) {
+  // Create a new image element
+  var image = document.createElement("img");
+  
+  // Set the image source and alt text
+  image.src = "./resources/img/RaelNeri/img" + " (" + i + ")" + ".jpg";
+  image.alt = "Card image";
+  image.className= "rounded";
+  // Create the necessary HTML structure
+  var colDiv = document.createElement("div");
+  colDiv.className = "col-md-3 pt-2";
+  
+  var cardDiv = document.createElement("div");
+  cardDiv.className = "card text-center bg-transparent shadow-lg border-0 rounded";
+  cardDiv.style.color = "#C3C0C0";
+  
+  // Append the image to the card div
+  cardDiv.appendChild(image);
+  
+  // Append the card div to the column div
+  colDiv.appendChild(cardDiv);
+  
+  // Append the column div to the container
+  container.appendChild(colDiv);
+}
+
+
+
+
+// Show or hide the "Back to Top" button based on the scroll position
+window.addEventListener("scroll", function() {
+  var backToTopButton = document.getElementById("back-to-top");
+  if (window.scrollY > 500) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+// Scroll to top when the "Back to Top" button is clicked
+var backToTopButton = document.getElementById("back-to-top");
+backToTopButton.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
