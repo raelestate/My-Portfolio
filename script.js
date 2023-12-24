1d33ec9563a5efea4011c48fe92598e67d8c3ae4
RaelEstate <rhyllneri@gmail.com>
2023-12-24 19:30:36 +0800

    minor changes

---
+++ script.js
@@ -0,0 +1,32 @@
+function generatesomething() {
+    const gene = document.getElementById("br");
+    const gen = document.createElement("br");
+    gen.classList.add(
+        "pt-5"
+    );
+    gene.appendChild(gen);
+    // gene1.appendChild(gen);
+    // gene2.appendChild(gen);
+    // gene3.appendChild(gen);
+    // gene4.appendChild(gen);
+    // gene5.appendChild(gen);
+    // gene6.appendChild(gen);
+    // gene7.appendChild(gen);
+    // gene8.appendChild(gen);
+    // gene9.appendChild(gen);
+    // gene10.appendChild(gen);
+    // gene11.appendChild(gen);
+    // gene12.appendChild(gen);
+    // gene13.appendChild(gen);
+    // gene14.appendChild(gen);
+    // gene15.appendChild(gen);
+    // gene16.appendChild(gen);
+    // gene17.appendChild(gen);
+    // gene18.appendChild(gen);
+    // gene19.appendChild(gen);
+    // gene20.appendChild(gen);
+
+  }
+  for(let i=0; i < 10; i++){
+    generatesomething();
+  }
